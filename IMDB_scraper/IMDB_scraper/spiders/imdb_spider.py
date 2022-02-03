1beaee9385e706d9bbe941a0c7fef255fa145b3c
ianmorris13 <97080872+ianmorris13@users.noreply.github.com>
2022-02-02 17:12:54 -0800

    initialize IMDB_scraper

---
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -0,0 +1,9 @@
+# to run 
+# scrapy crawl imdb_spider -o movies.csv
+
+import scrapy
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb_spider'
+    
+    start_urls = ['https://www.imdb.com/title/tt8714904/']
